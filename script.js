04ff10b7b233005fd72cd3b1708d8280165515ac
dino <40963149+del-alj@users.noreply.github.com>
2021-02-12 16:30:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function changeSize(rep)
+{
+    size = prompt("Enter new size", "");
+   //delete all content of container
+    container.innerHTML = '';
+    create(size);
+    return (size);
+}
+
+function create(size)
+{
+    let i = 0;
+    let rep = size * size;
+    let container = document.getElementById("container");
+    
+    while (i++ < rep)
+    {
+        // chage css
+        changeSizeOfDivs(size);
+        let div = ft_creatChild(i);
+        //style
+        ft_style(div, i, size);
+        
+        container.appendChild(div);
+
+        const tst = document.getElementById(div.id);
+        tst.addEventListener("mouseover", function (e) {
+            tst.style.backgroundColor = randomColor();
+        });
+    }
+}
+
+function   changeSizeOfDivs(size)
+{
+    // chage css size
+    var cssSize = document.querySelector(':root');
+    cssSize.style.setProperty('--size', size);
+}
+
+function ft_creatChild(i)
+{
+    let div = document.createElement('div', i);
+    div.id = "box" + i;
+    div.className = "border";
+    return div;
+}
+
+function ft_style(div, i , size)
+{
+    // let el = div.id;
+  
+
+    
+   
+    // div.addEventListener('mouseover', mouseOver);
+    div.style.width = mySize(size);
+    div.style.height = mySize(size);
+    div.style.gridColumn = i / size;
+    div.style.gridRow = i % size;
+    div.style.border.nameclass = "border";    
+}
+
+
+function randomColor()
+{
+    const color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    return color;
+}
+
+function mySize(size)
+{
+    return toString(size / 500) + "px";
+}
+
+
+
+create(16);
